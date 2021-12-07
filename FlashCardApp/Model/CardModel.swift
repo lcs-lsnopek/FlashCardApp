198003b555c43804bc9c98bd06801db3b7f83a25
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-07 14:30:09 -0500

    got some questions in the list

---
+++ FlashCardApp/Model/CardModel.swift
@@ -0,0 +1,30 @@
+//
+//  CardModel.swift
+//  FlashCardApp
+//
+//  Created by Logan Snopek on 2021-12-07.
+//
+
+import Foundation
+
+struct Card {
+    
+    let question: String
+    let awnser: String
+    
+}
+
+let listOfCards = [
+    Card(question: "What's \"power house\" of a cell",
+         awnser: "Miochondria")
+    
+    ,
+    
+    Card(question: "What is the largest bone in the human body",
+         awnser: "The ")
+    
+    ,
+    
+    Card(question: "What is the acceleration due to Gravity",
+         awnser: "9.80 meters a second")
+]

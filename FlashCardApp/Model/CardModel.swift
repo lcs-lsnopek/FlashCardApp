a2aabb2de9c85f89cface3d5d39776e9bb585d6f
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-07 15:34:04 -0500

    almost fix the duplacit question problem

--- FlashCardApp/Model/CardModel.swift
+++ FlashCardApp/Model/CardModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Card {
+struct Card: Equatable {
     
     let question: String
     let awnser: String
--- FlashCardApp/Views/ContentView.swift
+++ FlashCardApp/Views/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    
     //MARK: Strored Proprotes
-
+    
     @State var currentCard: Card = listOfCards.randomElement()!
-
+    
     @State var isAnswerShowing = false
-
+    
     //this is the prevouse Card
-    //@State var priorCard: Card = listOfCards.randomElement()!
-
+    @State var priorCard: Card = listOfCards.randomElement()!
+    
     //MARK: Computed Proprotes
     var body: some View {
         VStack{
@@ -46,17 +46,27 @@ struct ContentView: View {
             
             //imput
             Button(action: {
-                withAnimation {
+                
                 //hide the awnser
                 isAnswerShowing = false
+                //save the priorCard
+                priorCard = currentCard
+                
                 //pick a new Card
+                currentCard = listOfCards.randomElement()!
+                
+                // is the new card the same as the old Card
+                if currentCard == priorCard {
                     currentCard = listOfCards.randomElement()!
                 }
+                
             }, label: {
                 Text("New question")
             })
                 .buttonStyle(.bordered)
                 .padding()
+                .opacity(isAnswerShowing ? 1.0 : 0.0)
+            
             
             Spacer()
             

072226d09c42e43be5a623223bb2634965a6cbf7
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-07 14:14:21 -0500

    Initial Commit

---
+++ FlashCardApp/FlashCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardAppApp.swift
+//  FlashCardApp
+//
+//  Created by Logan Snopek on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
